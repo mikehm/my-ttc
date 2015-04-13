5c6814c25498fb963ad051c8ae10106b2994feb9
Mike <mike@veryona.com>
2015-04-13 19:06:47 -0400

    Created the basic UI. Modified schedulesController. Modified stylesheet.

--- modules/schedules/partial/schedules/schedules.js
+++ modules/schedules/partial/schedules/schedules.js
@@ -1,8 +1,17 @@
-angular.module('myTtc').controller('SchedulesCtrl',function($scope, scheduleService){
+angular.module('myTtc').controller('SchedulesCtrl',
+function($scope, scheduleService){
 
+	$scope.date_now = new Date();
+
+	$scope.stop_times = [];
 	scheduleService.getSched().then(
-			function(data){
-				$scope.ttc  = data;
-		});
+		function(data){
+		  $scope.routes = data;		
+			$scope.routes.forEach(function(item){
+				angular.forEach(item.stop_times, function(item){
+					$scope.stop_times.push(item);
+				})				
+			});
 
+	});
 });
--- shared/services/scheduleService/scheduleService.js
+++ shared/services/scheduleService/scheduleService.js
@@ -2,21 +2,18 @@ angular.module('myTtc').factory('scheduleService',function($http,$q) {
 
 	var scheduleService = {};
 	scheduleService.getSched = function getSched(station){
-
 		var deferred = $q.defer();	
-		$http.jsonp("http://myttc.ca/finch_station.json?callback=JSON_CALLBACK")
-			.success(function(data, status, headers, config){
-				
-				deferred.resolve(data);
-				
+		$http.jsonp("http://myttc.ca/eglinton_station.json?callback=JSON_CALLBACK")
+			.success(function(data, status, headers, config){			
+				angular.forEach(data.stops, function(item){
+					deferred.resolve(item.routes);
+				})
 			})
 			.error(function(err){
 				deferred.reject("Data is not available");
 			});
 		return deferred.promise;
 	};
-
-
 	return scheduleService;
 });
 
