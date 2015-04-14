1ccf71d225e3446d96f0576c69a3cf33b11195d3
Mike <mike@veryona.com>
2015-04-14 16:23:45 -0400

    Modified the view to include a form and search button. Modified the controller and service for schedules to allow for user input.

--- modules/schedules/partial/schedules/schedules.js
+++ modules/schedules/partial/schedules/schedules.js
@@ -1,17 +1,29 @@
 angular.module('myTtc').controller('SchedulesCtrl',
 function($scope, scheduleService){
-
+	
 	$scope.date_now = new Date();
-
+	
 	$scope.stop_times = [];
-	scheduleService.getSched().then(
+	
+	$scope.ask = function(station){
+
+
+		scheduleService.getSched(station).then(
 		function(data){
+		  $scope.message = '';
 		  $scope.routes = data;		
 			$scope.routes.forEach(function(item){
 				angular.forEach(item.stop_times, function(item){
-					$scope.stop_times.push(item);
-				})				
+					$scope.stop_times.push(item);			
+				});				
 			});
+		}, function(err){
+			$scope.message = "Data is not available for this station";
+		});
 
-	});
+		$scope.stop_times = [];	
+		$scope.station = '';
+	};
+
+
 });
--- shared/services/scheduleService/scheduleService.js
+++ shared/services/scheduleService/scheduleService.js
@@ -1,13 +1,14 @@
 angular.module('myTtc').factory('scheduleService',function($http,$q) {
 
 	var scheduleService = {};
+
 	scheduleService.getSched = function getSched(station){
 		var deferred = $q.defer();	
-		$http.jsonp("http://myttc.ca/eglinton_station.json?callback=JSON_CALLBACK")
+		$http.jsonp("http://myttc.ca/"+station+"_station.json?callback=JSON_CALLBACK")
 			.success(function(data, status, headers, config){			
 				angular.forEach(data.stops, function(item){
 					deferred.resolve(item.routes);
-				})
+				});
 			})
 			.error(function(err){
 				deferred.reject("Data is not available");
